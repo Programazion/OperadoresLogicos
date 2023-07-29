c6d6d07d26175f305b842c4b401e3179d7f15a46
Programazion <programazion1@gmail.com>
2023-07-29 18:28:18 -0500

    Validación exitosa

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 console.log('Loading JavaScript');
+//Validaciones del formaulario
 
 function validacion(){
     console.log('Validando');
     let name= document.getElementById('nombre').value;
     let email= document.getElementById('email').value;
     let tel= document.getElementById('telefono').value;
-    datos = [name,email,tel];
+    datos = [
+            'Nombre =>',name,
+             'email=>', email,
+             'telefono=>', tel];
     
-    if(datos){
-        alert('sus datos fueron enviados');
-        console.log('datos enviados', datos);       
+    if(datos[0]=="" || datos[1]=="" || datos[2]==""){
+        alert('faltan datos');      
     }else{
-        console.log('faltan datos');
-        
-        
+        alert('sus datos fueron enviados'); 
+        console.log('datos enviados', datos);   
     }
 }
